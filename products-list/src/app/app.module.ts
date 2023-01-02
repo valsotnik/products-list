1e7665fe062c20701ff401b7b2681753eaf48055
valery.sotnik <cmvaltime@gmail.com>
2023-01-02 12:06:45 +0100

    fix: fix npm packages conflict

--- products-list/src/app/app.module.ts
+++ products-list/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment.prod';
 import { reducers, metaReducers } from './reducers/index';
 import { ProductsModule } from './products/products.module';
 import { HomeComponent } from './pages/home/home.component';
@@ -9,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [ AppComponent, HomeComponent],
@@ -24,7 +26,8 @@ import { StoreModule } from '@ngrx/store';
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
