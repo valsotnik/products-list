2572efbce91c5971ef925149b70a18b3896765c5
valery.sotnik <cmvaltime@gmail.com>
2023-01-02 12:45:38 +0100

    feat: add reducers for implemented actions

--- products-list/src/app/products/store/index.ts
+++ products-list/src/app/products/store/index.ts
@@ -1,21 +1,43 @@
+import { loadProductsSuccess, loadProductsFailure } from './product.actions';
+import { Product } from './../models/products';
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
 import { environment } from 'src/environments/environment.prod';
 
 export const productStateFeatureKey = 'productState';
 
 export interface ProductState {
-
+  products: Product[];
+  error: any
 }
 
-export const reducers: ActionReducerMap<ProductState> = {
-
+export const initialState: ProductState = {
+  products: undefined,
+  error: undefined
 };
+
+export const reducers = createReducer(
+  initialState,
+  //@ts-ignore
+  on(loadProductsSuccess, (state, action) => {
+    return {
+      products: action.products
+    }
+  }),
+  on(loadProductsFailure, (state, action) => {
+    return {
+      products: state.products,
+      error: action.error
+    }
+  })
+)
 
 
 export const metaReducers: MetaReducer<ProductState>[] = !environment.production ? []: [];
