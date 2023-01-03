a2347a8c09019caad219eb14370e2ebeebadd3d9
valery.sotnik <cmvaltime@gmail.com>
2023-01-03 10:40:56 +0100

    tests: add unit tests for product list component

--- products-list/src/app/products/components/product-list/product-list.component.spec.ts
+++ products-list/src/app/products/components/product-list/product-list.component.spec.ts
@@ -1,28 +1,56 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule, Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import { ProductListComponent } from './product-list.component';
+import { ProductService } from '../../services/product.service';
+import * as fromActions from '../../store/product.actions';
+import { selectProducts } from '../../store/product.selectors';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProductListComponent ]
-    })
-    .compileComponents();
-  }));
+  let store: Store;
+  let service: ProductService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [ProductService, HttpClient, HttpHandler],
+      declarations: [ProductListComponent]
+    });
+
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.inject(Store);
+    service = TestBed.inject(ProductService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch a loadProducts action on init', () => {
+    spyOn(store, 'dispatch');
+
+    component.ngOnInit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(fromActions.loadProducts());
+  });
+
+  it('should delete a product', () => {
+    spyOn(store, 'dispatch');
+
+    component.deleteProduct('123');
+
+    expect(store.dispatch).toHaveBeenCalledWith(
+      fromActions.deleteProduct({ id: '123' })
+    );
   });
 });
